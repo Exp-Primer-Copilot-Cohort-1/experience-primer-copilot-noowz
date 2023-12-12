bc25c183844a665bd598a70b848e30b18785726e
Noowz <danieeel.dev@gmail.com>
2023-12-12 15:24:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var skills = [];
+    skills.push("JavaScript");
+    skills.push("HTML");
+    skills.push("CSS");
+    skills.push("Bootstrap");
+    skills.push("jQuery");
+    skills.push("Node.js");
+    skills.push("Express.js");
+    skills.push("React.js");
+    skills.push("Redux.js");
+    skills.push("MongoDB");
+    skills.push("MySQL");
+    skills.push("PostgreSQL");
+}
